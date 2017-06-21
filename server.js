af08df378f8da662cf0c8e9f6e11f49be7002f3c
System <none>
2017-06-21 10:31:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 // where your node app starts
 
 // init project
-const os = require('os');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 app.all("", (req, res)=>{
+  let ip = req.ip;
+  let platform=os.platform; 
+  let lang=os.;
   res.set("content-type", "application/json");
-  let data = JSON.stringify({"ip":0, "language": "en-UK", os:"windows"});
+  let data = JSON.stringify({"ip":ip, "language": lang, os:platform});
   res.send(data)
   res.end();
 });
