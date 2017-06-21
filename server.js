e3c22823c0650234510596b103dd66140adbbb0a
System <none>
2017-06-21 10:20:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const os = require('os');
 const express = require('express');
 const app = express();
 app.all("", (req, res)=>{
-  res.write("Hello, world!");
+  res.set("content-type", "application/json");
+  let data = JSON.stringify({"ip":0, "language": "en-UK", os:"windows"});
+  res.send(data)
+  res.end();
 });
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
