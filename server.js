a36f2340fd10c25508d4836111e8dd0f460be782
System <none>
2017-06-22 08:40:11 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 // where your node app starts
 
 // init project
-const os = require('useragent');
 const express = require('express');
 const app = express();
 app.all("", (req, res)=>{
+  let head = req.headers;
+  let os_ = head["accept-language"];
   let ip = req.ip;
-  let platform=os.platform; 
-  let lang;
+  let platform=head["user-agent"]; 
+  let lang = os_.substr(os_.indexOf("("), os_.indexOf(";"));
   res.set("content-type", "application/json");
   let data = JSON.stringify({"ip":ip, "language": lang, os:platform});
-  res.send(req.headers)
+  res.send(data)
   res.end();
 });
 var listener = app.listen(process.env.PORT, function () {
