f86a9acb5881012243e78a24e295c1772dc03d32
System <none>
2017-06-22 08:51:11 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-// server.js
-// where your node app starts
-
-// init project
+// set up app to use expressjs
 const express = require('express');
 const app = express();
+//respond
 app.all("", (req, res)=>{
   let head = req.headers;
-  let os_ = head["accept-language"];
-  let ip = req.ip;
-  let platform=head["user-agent"]; 
-  let lang = os_.substr(os_.indexOf("("), os_.indexOf(";"));
+  let ip = head["x-forwarded-for"].split(",")[0];
+  let os_ = head["user-agent"];
+  let os=os_.substring(os_.indexOf("(")+1, os_.indexOf(")"));
+  let lang = head["accept-language"].replace(/[\,\;]/g, " ").split(" ")[0];
   res.set("content-type", "application/json");
-  let data = JSON.stringify({"ip":ip, "language": lang, os:platform});
+  let data = JSON.stringify({"ip":ip, "language": lang, os:os});
   res.send(data)
   res.end();
 });
